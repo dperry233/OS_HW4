616afc6043991f5f040762147ef6a423055ed818
dperry233 <freemail40@yahoo.com>
2017-06-23 15:10:09 +0300

    untested find_syscall_table

--- intercept.c
+++ intercept.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 // TODO: add more #include statements as necessary
+#include <linux/utsname.h>
+
 
 MODULE_LICENSE("GPL");
 
@@ -10,7 +12,24 @@ void** sys_call_table = NULL;
 // TODO: import original syscall and write new syscall
 
 void find_sys_call_table(int scan_range) {
-   // TODO: complete the function
+   unsigned long **sysCallTable;
+   unsigned long ptrAsNum=(unsigned long)&system_utsname;
+   int i=0;
+   unsigned long *ptr;
+   sysCallTable = NULL;
+   for (i=0; i<scan_range,i++)
+   {
+	 ptrAsNum += sizeof(void *)
+	 ptr = (unsigned long *)ptrAsNum;
+	  
+     if (ptr == &sys_read())
+     {
+        sysCallTable = (unsigned long **)ptr;  //read is actually number 3, so might want to return num0 or num1
+		return &sysCallTable[0];
+     }
+   }
+
+   return NULL;
 }
 
 int init_module(void) {
