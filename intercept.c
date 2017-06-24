d2142f76fc12e3fd31f610669192aa71b47495c2
dperry233 <freemail40@yahoo.com>
2017-06-24 12:53:29 +0300

    compiling with getpid instead of unlink

--- intercept.c
+++ intercept.c
@@ -2,43 +2,58 @@
 #include <linux/kernel.h>
 // TODO: add more #include statements as necessary
 #include <linux/utsname.h>
-
-
+#include <sys/syscall.h>
+#include <linux/syscall.h>
 MODULE_LICENSE("GPL");
 
 // TODO: add command-line arguments
 void** sys_call_table = NULL;
 
 // TODO: import original syscall and write new syscall
-
+int scan_range;
+MODULE_PARM(scan_range,"i");
 void find_sys_call_table(int scan_range) {
    
    unsigned long ptrAsNum=(unsigned long)&system_utsname;
    int i=0;
    unsigned long *ptr;
-   sysCallTable = NULL;
-   for (i=0; i<scan_range,i++)
+   sys_call_table = NULL;
+   for (i=0; i<scan_range; i++)
    {
 	 ptrAsNum += sizeof(void *);
 	 ptr = (unsigned long *)ptrAsNum;
 	  
-     if (ptr == &sys_read())
+     if (ptr == (unsigned long *)&sys_read)
      {
-		ptrAsNum -= sizeof(void *)*3;  //since sys_read is number 3 in the table
+		ptrAsNum -= sizeof(void *)*__NR_read;  
 		 ptr = (unsigned long *)ptrAsNum;
-        sys_call_table = (unsigned long **)ptr;  
+        sys_call_table = (void **)ptr;  
 		
      }
    }
-
+   printk("scan_range to low ,didnt find sys_call_table\n");
    
 }
 
+//this is just a test syscall (which is getpid) should be changed to unlink
+asmlinkage long (*original_sys_getpid)(void);
+asmlinkage long our_sys_getpid(void) {
+    printk("sys_getpid was called\n");
+    /*call original syscall and return its value*/
+    return original_sys_getpid();
+}
+
+
 int init_module(void) {
    // TODO: complete the function
+   find_sys_call_table(scan_range);
+   original_sys_getpid=sys_call_table[__NR_getpid];
+   return 0;
 }
 
 void cleanup_module(void) {
    // TODO: complete the function
+
+   sys_call_table[__NR_getpid] = original_sys_getpid;
 }
 
