c4e310c6f3dc8d7430e558660cbb727bdc82e10e
dperry233 <freemail40@yahoo.com>
2017-06-24 16:29:08 +0300

    fix for find_syscall

--- intercept.c
+++ intercept.c
@@ -13,27 +13,24 @@ void** sys_call_table = NULL;
 int scan_range=150;
 MODULE_PARM(scan_range,"i");
 void find_sys_call_table(int scan_range) {
-   //printk("scan range is %d\n",scan_range);//dbg
    unsigned long ptrAsNum=(unsigned long)&system_utsname;
    int i=0;
    unsigned long *ptr;
    sys_call_table = NULL;
    for (i=0; i<scan_range; i++)
    {
-	//printk("i is %d\n",i);//dbg
 	 ptrAsNum += sizeof(void *);
 	 ptr = (unsigned long *)ptrAsNum;
 	  
      if (ptr[__NR_read] == (unsigned long )sys_read)
      {
-		ptrAsNum -= sizeof(void *)*__NR_read;  
+
 		 ptr = (unsigned long *)ptrAsNum;
         sys_call_table = (void **)ptr; 
-		//printk("successfully found sys_call_table\n");//dbg
+
 		return;
      }
    }
-   //printk("scan_range too low ,didnt find sys_call_table\n");
    
 }
 
@@ -50,6 +47,7 @@ int init_module(void) {
    // TODO: complete the function
    find_sys_call_table(scan_range);
    original_sys_getpid=sys_call_table[__NR_getpid];
+   sys_call_table[__NR_getpid]=our_sys_getpid;
    return 0;
 }
 
